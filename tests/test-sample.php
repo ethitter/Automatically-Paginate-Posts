9dad509983f07d3bab9076260a879cd46a38bc4f
Erick Hitter <git-contrib@ethitter.com>
2019-04-14 15:02:01 -0700

    Add scaffold

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Automatically_Paginate_Posts
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
